269ccfe98b1204fb7d9515540f7f0e7ba03f5e7b
Laveytex <YanAlex13@mail.ru>
2023-03-06 22:44:21 +0400

    Com_08
    1. Create GameInstance. Addade function for work with table.

---
+++ Source/TDS/TDSGameInstance.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MainTypes.h"
+#include "MainTypes.h"
+#include "Engine/DataTable.h"
+#include "Engine/GameInstance.h"
+#include "TDSGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TDS_API UTDSGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "WeaponSetting")
+	UDataTable* WeaponInfoTable = nullptr;
+	UFUNCTION(BlueprintCallable)
+	bool GetWeaponInfoByName(FName NameWeapon, FWeaponInfo& OutInfo);
+};
